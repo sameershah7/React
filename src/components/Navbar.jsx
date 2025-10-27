9828ab35df222d6af283b2a99f8ecfe21aeb4f69
Sameer Shah <shahkhanpc8925@gmail.com>
2025-10-27 22:09:02 +0500

    Create a simple navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar(){
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <span>Navbar</span>
+            </div>
+            <div className="nav-item">
+                <ul>
+                    <li>Projects</li>
+                    <li>About us</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
